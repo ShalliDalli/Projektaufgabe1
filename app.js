c9052d6f229fb11068816d19ac3a8dc3d50c69db
shallidalli <shallidalli@web.de>
2021-03-23 10:55:47 +0100

    Update 1.2
    Maybe Final Update

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
-var tl = new TimelineMax({onUpdate:updatePercentage});
+var tl = new TimelineMax();
+var tl2 = new TimelineMax();
+var tl3 = new TimelineMax();
+
 const controller = new ScrollMagic.Controller();
+const controller2 = new ScrollMagic.Controller();
+const controller3 = new ScrollMagic.Controller();
 
 tl.from("#Text1", .5, {x:200, opacity: 0}),
 tl.from("#Bild1", .5, {x:-200, opacity: 0}),
-tl.from("#Text2", .5, {x:200, opacity: 0}),
-tl.from("#Bild2", .5, {x:-200, opacity: 0}),
-tl.from("#Text3", .5, {x:200, opacity: 0}),
-tl.from("#Bild3", .5, {x:-200, opacity: 0});
+tl.from("#Bild2", .5, {x:-200, opacity:0});
 
+tl2.from("#Text1_", .5, {x:200, opacity: 0}),
+tl2.from("#Bild1_", .5, {x:-200, opacity: 0}),
+tl2.from("#Text2_", .5, {x:200, opacity: 0}),
+tl2.from("#Bild2_", .5, {x:-200, opacity: 0}),
+tl2.from("#Text3_", .5, {x:200, opacity: 0}),
+tl2.from("#Bild3_", .5, {x:-200, opacity: 0});
+
+tl3.from("#Telefon1", .5, {x:200,opacity: 0}),
+tl3.from("#Telefon2", .5, {x:-200,opacity: 0});
 
 const scene = new ScrollMagic.Scene({
 
@@ -22,8 +33,66 @@ duration: "100%"
 .setTween(tl)
 .addTo(controller);
 
-function updatePercentage(){
+const scene2 = new ScrollMagic.Scene({
 
-    tl.progress();
-    console.log(tl.progress());
-}
+
+    triggerElement: "#Section_Allgemeines",
+    triggerHook:"onLeave",
+    duration: "100%"
+    
+    })
+
+    .setPin("#Section_Allgemeines")
+.setTween(tl2)
+.addTo(controller2);
+
+const scene3 = new ScrollMagic.Scene({
+
+
+triggerElement: "#Section_Telefone",
+triggerHook:"onLeave",
+duration:"100%"
+
+})
+
+.setPin("#Section_Telefone")
+.setTween(tl3)
+.addTo(controller3);
+
+
+var t1 = new TimelineMax({paused: true});
+
+t1.to(".one", 0.8, {
+     y: 6,
+     rotation: 45,
+     ease: Expo.easeInOut
+});
+
+t1.to(".two", 0.8, {
+     y: -6,
+     rotation: -45,
+     ease: Expo.easeInOut,
+     delay: -0.8
+});
+
+t1.to(".menu", 2, {
+     top: "0%",
+     ease: Expo.easeInOut,
+     delay: -2
+});
+
+t1.staggerFrom(".menu ul li", 2, {x: -200, opacity: 0, ease:Expo.easeOut}, 0.3);
+
+t1.reverse();
+
+$(document).on("click", ".toggle-btn", function() {
+     t1.reversed(!t1.reversed());
+});
+$(document).on("click", "a", function() {
+     t1.reversed(!t1.reversed());
+});
+
+
+
+
+
